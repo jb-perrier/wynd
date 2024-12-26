bd5bce192d38dc1065d18de98f5d0f679f18564d
Jean-Baptiste Perrier <jean-baptiste.perrier@esker.com>
2024-12-26 09:16:27 +0100

    Add devcontainer + move out of folder

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,24 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  pkg-config \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl \
+  libssl-dev
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+
+## setup and install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# cp -R /root/.oh-my-zsh /home/$USERNAME
+# cp /root/.zshrc /home/$USERNAME
+# sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+# chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
---
+++ .devcontainer/setup_cargo.sh
@@ -0,0 +1,8 @@
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
